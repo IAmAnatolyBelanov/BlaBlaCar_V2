21bc60e0e0039bb9926116b105b8b82cba0d15fe
DESKTOP-AC9U0SH\Anatoly <anatoly.belanov.0798@gmail.com>
2024-01-08 11:34:32 +0300

    Test constraint

--- src/WebApi/DataAccess/ApplicationContext.cs
+++ src/WebApi/DataAccess/ApplicationContext.cs
@@ -71,11 +71,28 @@ namespace WebApi.DataAccess
 				builder.HasIndex(x => new { x.LegId, x.UserId })
 					.IsUnique()
 					.HasFilter($"\"{nameof(Reservation.IsActive)}\" IS TRUE")
-					.HasDatabaseName(DbConstants.IndexName_UniqueIfActiveReservation);
+					.HasDatabaseName(DbConstants.IndexName_Reservation_UniqueIfActive);
 
 				builder.HasOne(x => x.Leg)
 					.WithMany()
 					.HasForeignKey(x => x.LegId);
+
+				//				// https://chat.openai.com/c/23535a19-c965-4287-b4d1-acc488ec1ef3
+				//				builder.ToTable(b
+				//					=> b.HasCheckConstraint(DbConstants.ConstraintName_Reservation_EnoughFreePlaces, $@"
+				//(SELECT SUM(reserv.""{nameof(Reservation.Count)}"") FROM ""{nameof(ApplicationContext.Rides)}"" AS ride
+				//JOIN ""{nameof(ApplicationContext.Legs)}"" AS leg ON leg.""{nameof(Leg.RideId)}"" = ride.""{nameof(Ride.Id)}""
+				//JOIN ""{nameof(ApplicationContext.Reservations)}"" reserv ON reserv.""{nameof(Reservation.LegId)}"" = leg.""{nameof(Leg.Id)}"" AND reserv.""{nameof(Reservation.IsActive)}"" IS TRUE AND reserv.""{nameof(Reservation.Id)}"" = ""{nameof(Reservation.Id)}"")
+				//<=
+				//(SELECT MAX(ride.""{nameof(Ride.AvailablePlacesCount)}"") FROM ""{nameof(ApplicationContext.Rides)}"" AS ride
+				//JOIN ""{nameof(ApplicationContext.Legs)}"" AS leg ON leg.""{nameof(Leg.RideId)}"" = ride.""{nameof(Ride.Id)}""
+				//JOIN ""{nameof(ApplicationContext.Reservations)}"" reserv ON reserv.""{nameof(Reservation.LegId)}"" = leg.""{nameof(Leg.Id)}"" AND reserv.""{nameof(Reservation.IsActive)}"" IS TRUE AND reserv.""{nameof(Reservation.Id)}"" = ""{nameof(Reservation.Id)}"")
+				//"));
+
+				//				builder.ToTable(t =>
+				//				{
+				//					t.HasTrigger()
+				//				});
 			});
 		}
 	}
@@ -88,6 +105,10 @@ namespace WebApi.DataAccess
 		public static readonly IReadOnlySet<string> AllConstantValues
 			= AllConstants.Values.ToHashSet();
 
-		public const string IndexName_UniqueIfActiveReservation = "Custom_DbIndexName_Reservations_UserId_LegId_UniqueIfActive";
+		public const string IndexName_Reservation_UniqueIfActive
+			= "Custom_DbIndexName_Reservations_UserId_LegId_UniqueIfActive";
+
+		public const string ConstraintName_Reservation_EnoughFreePlaces
+			= "Custom_DbConstraintName_Reservations_EnoughFreePlaces";
 	}
 }
