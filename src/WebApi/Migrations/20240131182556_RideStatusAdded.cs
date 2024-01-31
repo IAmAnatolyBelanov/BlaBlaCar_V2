dcd3d5172c7844b036e9b9c6f2eef4282878fa06
DESKTOP-VS1KLJK\Anatoly <anatoly.belanov.0798@gmail.com>
2024-01-31 21:26:55 +0300

    Added migrations with RideStatus

---
+++ src/WebApi/Migrations/20240131182556_RideStatusAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RideStatusAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Rides",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Rides");
+        }
+    }
+}
